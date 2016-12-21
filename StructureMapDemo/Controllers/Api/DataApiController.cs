b3b186b7e1f57fdf398e8146045e1a0ed5ec65f0
Vladimir <v.pavlunin@paralect.com>
2016-12-21 13:42:18 +0300

    initial commit

---
+++ StructureMapDemo/Controllers/Api/DataApiController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Http;
+
+namespace StructureMapDemo.Controllers.Api
+{
+    public class DataApiController : ApiController
+    {
+        [HttpGet]
+        public Person GetPerson()
+        {
+            return new Person { Name = "Ivan", Age = 28 };
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
